0cdcb5e7ce5eb1b654f6275fdb7c6b0859bdbc99
dilraton <1418334540@qq.com>
2019-09-17 10:59:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView out;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test);
+
+        out = findViewById(R.id.out);
+        out.setText("Hello kkkkk");
+
+        EditText inp = findViewById(R.id.inp);
+        String str = inp.getText().toString();
+        inp.setText("123456");
+
+        Button btn = findViewById(R.id.but);
+        btn.setOnClickListener(this);
+
+
+    }
+    public void onClick(View v){
+        out.setText("AAAAAAAAA");
+    }
+}
